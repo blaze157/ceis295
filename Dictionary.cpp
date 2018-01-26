b20db7dd77e815e36d07d268cf73fa10ac4468e7
Wesley Salesberry <Wesley.salesberry@hotmail.com>
2018-01-26 16:56:37 -0500

    Update Dictionary.cpp

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,13 +1,21 @@
 #include "Dictionary.h"
 
-int Dictionary::hash(std::string word)
-{
-	//make sure strings are converted to lower case to
-	//avoid missing correct words due to case difference
 
-	return 0;//obviously not finished yet
+//Thinking it might work better as a function in main?
+template <class Word>
+std::string Dictionary::To_Lower_Case(std::string word)
+{
+	//converting everyword to its lowercase
+	for (unsigned c = 0; c < word.length(); ++c)
+	{
+		//using tolower functio for lower case conversion
+		word[c] = tolower(word[c]);
+	}
+	//returning the lowercase word
+	return word;
 }
 
+template <class Word>
 void Dictionary::addWord(std::string word)
 {
 	Word newWord;
@@ -17,6 +25,8 @@ void Dictionary::addWord(std::string word)
 
 	words.push_back(newWord);
 }
+
+template <class Word>
 bool Dictionary::validWord(std::string word)
 {
 	int wordHash = hash(word);
@@ -27,6 +37,62 @@ bool Dictionary::validWord(std::string word)
 			return true;
 		}
 	}
-
 	return false;
+}
+
+template <class Word>
+bool Dictionary<Word>::IsEmpty(int KeyWord)
+{
+	if (key >= 0 && key < hashSize)
+	{
+		return head[key] == NULL;
+	}
+	return true;
+}
+
+template<class Word>
+bool Dictionary<Word>::IsFull()
+{
+	try
+	{
+		node* location = new node;
+		delete location;
+		return false;
+	}
+	catch (std::bad_alloc&)
+	{
+		return true;
+	}
+}
+
+template<class Word>
+void Dictionary<Word>::Append(int key, Word NewWord)
+{
+	if (IsFull())
+	{
+		//std::cout<<"nAPPEND ERROR - HASH TABLE FULLn";
+	}
+	else
+	{
+		node* newNode = new node; // adds new node
+		newNode->currentItem = newItem;
+		newNode->next = NULL;
+		if (IsEmpty(key))
+		{
+			head[key] = newNode;
+		}
+		else
+		{
+			node* tempPtr = head[key];
+
+			while (tempPtr->next != NULL)
+			{
+				tempPtr = tempPtr->next;
+			}
+			tempPtr->next = newNode;
+		}
+		++bucketSize[key];
+		++totElems;
+	}
+
 }
